5bb0b00b4f71b9a8f771b3c318937b2b126c7d4a
JVAS42 <jvasprogramando@gmail.com>
2025-10-13 11:18:57 -0300

    Exercicio de set()

--- teste.py
+++ teste.py
@@ -1,1 +1,7 @@
-print('Ola teste')
+lista_de_inteiros = [9, 1, 8, 9, 9, 7, 2, 1, 6, 8, 10, 11]
+
+print(lista_de_inteiros)
+
+for num in lista_de_inteiros:
+    print(lista_de_inteiros)
+    print(num)
